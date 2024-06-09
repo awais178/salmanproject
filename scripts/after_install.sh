88e66505c317ff9bc29f2acade48ac93c8277c8c
Awais khan <139611969+awais178@users.noreply.github.com>
2024-06-09 14:56:34 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Install dependencies or perform other setup tasks
 
-# Check if the docker-compose.yml file exists in the root directory
+# Change to the directory where the docker-compose.yml file is located
+cd "$(dirname "$0")"
+
+# Check if the docker-compose.yml file exists in the current directory
 if [ -f docker-compose.yml ]; then
     # If the file exists, pull Docker images using docker-compose
     docker-compose pull
@@ -10,4 +13,3 @@ else
     echo "Error: docker-compose.yml file not found."
     exit 1
 fi
-
