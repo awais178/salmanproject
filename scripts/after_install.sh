ed9f7aa59f6be9348c9b53f235a52b51003fc9d1
Awais khan <139611969+awais178@users.noreply.github.com>
2024-06-09 15:11:41 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Install dependencies or perform other setup tasks
 
-# Change to the directory where the docker-compose.yml file is located
-cd "$(dirname "$0")"
+# Change directory to where docker-compose.yml is located
+cd /home/ubuntu/newdocker-project
 
-# Check if the docker-compose.yml file exists in the current directory
-if [ -f docker-compose.yml ]; then
-    # If the file exists, pull Docker images using docker-compose
-    docker-compose pull
-else
-    # If the file does not exist, print an error message
-    echo "Error: docker-compose.yml file not found."
-    exit 1
-fi
+# Build Docker images using Docker Compose
+docker-compose -f docker-compose.yml build
+
+# Push Docker images to Docker Hub
+docker-compose -f docker-compose.yml push
